fe2d0aa0a40a86dce1bebcb7ead3a96963c8bce3
zac-heisey <zheisey@gmail.com>
2016-04-13 10:31:34 -0400

    Small jQuery adjustments

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // Toggle > Initial Content & About Me
 $('.about-me').hide();
+$('.get-in-touch').hide();
+
 $('#btn-about').click(function() {
   $('.initial-content, .about-me').toggle("slide", {direction: "right"}, 800);
 });
 
 // Toggle > Initial Content & Get In Touch
-$('.get-in-touch').hide();
 $('#btn-contact').click(function() {
   $('.initial-content, .get-in-touch').toggle("slide", {direction: "right"}, 800);
 });
