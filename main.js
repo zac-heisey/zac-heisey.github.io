73d12404d838853c23846d4928f7e5c1d842062d
Zac Heisey <zheisey@gmail.com>
2016-09-13 20:14:03 -0400

    fix overlapping toggle issue on homepage content section

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ $('.get-in-touch').hide();
 
 // Toggle > Initial Content & About Me
 $('#btn-about').click(function() {
-  $('.initial-content, .about-me').toggle("slide", {direction: "right"}, 800);
+  if ($('.get-in-touch').is(':visible')) {
+    $('.get-in-touch, .about-me').toggle('slide', {direction: 'right'}, 800);
+  } else {
+    $('.initial-content, .about-me').toggle('slide', {direction: 'right'}, 800);
+  }
 });
 
 // Toggle > Initial Content & Get In Touch
 $('#btn-contact').click(function() {
-  $('.initial-content, .get-in-touch').toggle("slide", {direction: "right"}, 800);
+  if ($('.about-me').is(':visible')) {
+    $('.about-me, .get-in-touch').toggle('slide', {direction: 'right'}, 800);
+  } else {
+    $('.initial-content, .get-in-touch').toggle('slide', {direction: 'right'}, 800);
+  }
 });
