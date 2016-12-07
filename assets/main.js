8aa64fa76d12e96309feac8a1e089f43ce232d37
Zac Heisey <zheisey@gmail.com>
2016-12-06 20:57:17 -0500

    update homepage text and add new Certifications button + section

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,36 @@
 $('.about-me').hide();
 $('.get-in-touch').hide();
+$('.certifications').hide();
 
-// Toggle > Initial Content & About Me
+// Show About Me Content
 $('#btn-about').click(function() {
   if ($('.get-in-touch').is(':visible')) {
-    $('.get-in-touch, .about-me').toggle('slide', {direction: 'right'}, 800);
+    $('.get-in-touch, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & About Me
+  } else if ($('.certifications').is(':visible')) {
+    $('.certifications, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Certifications
   } else {
-    $('.initial-content, .about-me').toggle('slide', {direction: 'right'}, 800);
+    $('.initial-content, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
   }
 });
 
-// Toggle > Initial Content & Get In Touch
+// Show Get In Touch Content
 $('#btn-contact').click(function() {
   if ($('.about-me').is(':visible')) {
-    $('.about-me, .get-in-touch').toggle('slide', {direction: 'right'}, 800);
+    $('.about-me, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & Get In Touch
+  } else if ($('.certifications').is(':visible')) {
+    $('.certifications, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Certifications & Get In Touch
   } else {
-    $('.initial-content, .get-in-touch').toggle('slide', {direction: 'right'}, 800);
+    $('.initial-content, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
+  }
+});
+
+// Show Certifications Content
+$('#btn-certifications').click(function() {
+  if ($('.about-me').is(':visible')) {
+    $('.about-me, .certifications').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & Certifications
+  } else if ($('.get-in-touch').is(':visible')) {
+    $('.get-in-touch, .certifications').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Certifications
+  } else {
+    $('.initial-content, .certifications').toggle('slide', {direction: 'right'}, 800); // Toogle > Initial Content & Certifications
   }
 });
