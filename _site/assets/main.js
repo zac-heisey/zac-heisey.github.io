63a03b132677814cac9850e35e2724c9d30568c9
Zac Heisey <zheisey@gmail.com>
2017-05-29 12:25:00 -0400

    revamp homepage to be nonprofit web developer-centric

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -1,36 +1,35 @@
-// $('.about-me').hide();
 $('.get-in-touch').hide();
-$('.certifications').hide();
+$('.projects').hide();
 
-// Show About Me Content
+// Show 'About Me' Content
 $('#btn-about').click(function() {
   if ($('.get-in-touch').is(':visible')) {
     $('.get-in-touch, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & About Me
-  } else if ($('.certifications').is(':visible')) {
-    $('.certifications, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Certifications
+  } else if ($('.projects').is(':visible')) {
+    $('.projects, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Projects
   } else {
     $('.initial-content, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
   }
 });
 
-// Show Get In Touch Content
+// Show 'Get In Touch' Content
 $('#btn-contact').click(function() {
   if ($('.about-me').is(':visible')) {
     $('.about-me, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & Get In Touch
-  } else if ($('.certifications').is(':visible')) {
-    $('.certifications, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Certifications & Get In Touch
+  } else if ($('.projects').is(':visible')) {
+    $('.projects, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Projects & Get In Touch
   } else {
     $('.initial-content, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
   }
 });
 
-// Show Certifications Content
-$('#btn-certifications').click(function() {
+// Show 'Projects' Content
+$('#btn-projects').click(function() {
   if ($('.about-me').is(':visible')) {
-    $('.about-me, .certifications').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & Certifications
+    $('.about-me, .projects').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & projects
   } else if ($('.get-in-touch').is(':visible')) {
-    $('.get-in-touch, .certifications').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Certifications
+    $('.get-in-touch, .projects').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & projects
   } else {
-    $('.initial-content, .certifications').toggle('slide', {direction: 'right'}, 800); // Toogle > Initial Content & Certifications
+    $('.initial-content, .projects').toggle('slide', {direction: 'right'}, 800); // Toogle > Initial Content & projects
   }
 });
