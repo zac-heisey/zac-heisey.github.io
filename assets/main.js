bad55e859290bf7db4c7813d5c114178cff94d22
Zac Heisey <zheisey@gmail.com>
2017-05-29 12:39:15 -0400

    clean up JavaScript if/else if statements

--- assets/main.js
+++ assets/main.js
@@ -7,8 +7,6 @@ $('#btn-about').click(function() {
     $('.get-in-touch, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & About Me
   } else if ($('.projects').is(':visible')) {
     $('.projects, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & Projects
-  } else {
-    $('.initial-content, .about-me').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
   }
 });
 
@@ -18,8 +16,6 @@ $('#btn-contact').click(function() {
     $('.about-me, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & Get In Touch
   } else if ($('.projects').is(':visible')) {
     $('.projects, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Projects & Get In Touch
-  } else {
-    $('.initial-content, .get-in-touch').toggle('slide', {direction: 'right'}, 800); // Toggle > Initial Content & Get In Touch
   }
 });
 
@@ -29,7 +25,5 @@ $('#btn-projects').click(function() {
     $('.about-me, .projects').toggle('slide', {direction: 'right'}, 800); // Toggle > About Me & projects
   } else if ($('.get-in-touch').is(':visible')) {
     $('.get-in-touch, .projects').toggle('slide', {direction: 'right'}, 800); // Toggle > Get In Touch & projects
-  } else {
-    $('.initial-content, .projects').toggle('slide', {direction: 'right'}, 800); // Toogle > Initial Content & projects
   }
 });
