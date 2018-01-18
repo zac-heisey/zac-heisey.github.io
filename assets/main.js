3be98d8bb26c158b6b647215294ba8b56d6f48a8
Zac Heisey <zheisey@gmail.com>
2018-01-18 18:15:59 -0500

    work on slide-out nav menu

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,6 @@
-$('#about-section').hide();
-$('#work-section').hide();
+$('#site-nav').hide();
 
-// Show Intro Section
-$('#nav-intro').click(function() {
-  if ($('#about-section').is(':visible')) {
-    $('#about-section, #intro-section').toggle('slide', {direction: 'right'}, 800); // Toggle > About & Intro
-  } else if ($('#work-section').is(':visible')) {
-    $('#work-section, .#intro-section').toggle('slide', {direction: 'right'}, 800); // Toggle > Work & Intro
-  }
-});
-
-// Show About Section
-$('#nav-about').click(function() {
-  if ($('#intro-section').is(':visible')) {
-    $('#intro-section, #about-section').toggle('slide', {direction: 'right'}, 800); // Toggle > Intro & About
-  } else if ($('#work-section').is(':visible')) {
-    $('#work-section, #about-section').toggle('slide', {direction: 'right'}, 800); // Toggle > Work & About
-  }
-});
-
-// Show Work Section
-$('#nav-work').click(function() {
-  if ($('#about-section').is(':visible')) {
-    $('#about-section, #work-section').toggle('slide', {direction: 'right'}, 800); // Toggle > About & Work
-  } else if ($('#intro-section').is(':visible')) {
-    $('#intro-section, #work-section').toggle('slide', {direction: 'right'}, 800); // Toggle > Intro & Work
-  }
+// Show Nav Menu
+$('#nav-btn').click(function() {
+  $('#site-nav').slideToggle(600); // Toggle Nav Menu (slides from top to bottom)
 });
