52e961783cf80764af7cb208103eda6865db6ba1
Zac Heisey <zheisey@gmail.com>
2018-01-19 20:15:01 -0500

    hide nav menu on X CLOSE click

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,8 @@ $('#site-nav').hide();
 $('#nav-btn').click(function() {
   $('#site-nav').slideToggle(600); // Toggle Nav Menu (slides from top to bottom)
 });
+
+// Hide Nav Menu
+$('#close-nav').click(function() {
+  $('#site-nav').slideToggle(600); // Toggle Nav Menu (slides from top to bottom)
+});
